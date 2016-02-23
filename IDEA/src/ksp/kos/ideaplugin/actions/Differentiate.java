37c1560cb8452a16b7bc9452929abe51b5d81ed7
Vladislav Rassokhin <vladrassokhin@gmail.com>
2016-02-24 00:40:57 +0300

    Proper errors reporting and simplification in actions

--- IDEA/src/ksp/kos/ideaplugin/actions/Differentiate.java
+++ IDEA/src/ksp/kos/ideaplugin/actions/Differentiate.java
@@ -1,10 +1,14 @@
 package ksp.kos.ideaplugin.actions;
 
+import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.lang.ASTFactory;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import ksp.kos.ideaplugin.expressions.Expression;
@@ -17,33 +21,62 @@ import ksp.kos.ideaplugin.psi.*;
  * @author ptasha
  */
 public class Differentiate extends BaseAction {
+    private static final Logger LOG = Logger.getInstance(Differentiate.class);
+
     @Override
     public void actionPerformed(AnActionEvent event) {
         final Project project = getEventProject(event);
         if (project == null) return;
-        KerboScriptExpr expr = getDifferentiable(event);
-        if (expr != null) {
-            WriteCommandAction.runWriteCommandAction(project, () -> {
-                try {
-                    KerboScriptInstruction parent = PsiTreeUtil.getParentOfType(expr, KerboScriptInstruction.class, false);
-                    if (parent == null) return;
-                    if (!(parent instanceof KerboScriptReturnStmt)) {
-                        PsiElement copy = parent.copy();
-                        copy.getNode().addChild(ASTFactory.whitespace("\n"));
-                        parent.getParent().addBefore(copy, parent);
-                        if (parent instanceof KerboScriptDeclareStmt) {
-                            KerboScriptDeclareIdentifierClause identifier = ((KerboScriptDeclareStmt) parent).getDeclareIdentifierClause();
-                            if (identifier!=null) {
-                                identifier.rawRename(identifier.getRawName()+"_");
-                            }
-                        }
-                    }
-                    expr.replace(KerboScriptElementFactory.expression(project, diff(expr)));
-                } catch (ActionFailedException e) {
-                    Messages.showErrorDialog(project, "Exception:" +e.getMessage(), "Differentiate Action Failed");
-                }
-            });
+        final KerboScriptExpr expr = getDifferentiable(event);
+        if (expr == null) return;
+        final Editor editor = event.getData(CommonDataKeys.EDITOR);
+        if (editor == null) return;
+        LOG.debug("Differentiating: " + expr.getText());
+        // First, prepare new statement
+        final String differentiated;
+        try {
+            differentiated = diff(expr);
+        } catch (ActionFailedException e) {
+            final String message = "Cannot differentiate expression. " + e.getMessage();
+            LOG.warn(message, e);
+            HintManager.getInstance().showErrorHint(editor, message);
+            return;
         }
+        LOG.debug("Differentiated to: " + differentiated);
+        if (StringUtil.isEmptyOrSpaces(differentiated)) {
+            final String message = "Differentiation result is empty for " + expr.getText();
+            LOG.warn(message);
+            HintManager.getInstance().showErrorHint(editor, message);
+            return;
+        }
+        final KerboScriptExpr expression = KerboScriptElementFactory.expression(project, differentiated);
+        if (expression == null) {
+            final String message = "Cannot construct expression for input '" + differentiated + "'";
+            LOG.error(message);
+            HintManager.getInstance().showErrorHint(editor, message);
+            return;
+        }
+
+        // Then update existing model
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            KerboScriptInstruction parent = PsiTreeUtil.getParentOfType(expr, KerboScriptInstruction.class, false);
+            // Duplicate original statement and rename it if it's not return statement
+            if (parent == null) {
+                LOG.debug("Unexpected: parent is null for expression ", expr.getText());
+            }
+            if (parent != null && !(parent instanceof KerboScriptReturnStmt)) {
+                PsiElement copy = parent.copy();
+                copy.getNode().addChild(ASTFactory.whitespace("\n"));
+                parent.getParent().addBefore(copy, parent);
+                if (parent instanceof KerboScriptDeclareStmt) {
+                    KerboScriptDeclareIdentifierClause identifier = ((KerboScriptDeclareStmt) parent).getDeclareIdentifierClause();
+                    if (identifier != null) {
+                        identifier.rawRename(identifier.getRawName() + "_");
+                    }
+                }
+            }
+            expr.replace(expression);
+        });
     }
 
     private String diff(KerboScriptExpr expr) throws ActionFailedException {
--- IDEA/src/ksp/kos/ideaplugin/actions/Simplify.java
+++ IDEA/src/ksp/kos/ideaplugin/actions/Simplify.java
@@ -1,12 +1,18 @@
 package ksp.kos.ideaplugin.actions;
 
+import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.util.PsiTreeUtil;
 import ksp.kos.ideaplugin.expressions.Expression;
 import ksp.kos.ideaplugin.expressions.SyntaxException;
-import ksp.kos.ideaplugin.psi.*;
+import ksp.kos.ideaplugin.psi.ExpressionHolder;
+import ksp.kos.ideaplugin.psi.KerboScriptElementFactory;
+import ksp.kos.ideaplugin.psi.KerboScriptExpr;
 
 /**
  * Created on 24/01/16.
@@ -14,19 +20,28 @@ import ksp.kos.ideaplugin.psi.*;
  * @author ptasha
  */
 public class Simplify extends BaseAction {
+    private static final Logger LOG = Logger.getInstance(Simplify.class);
+
     @Override
     public void actionPerformed(AnActionEvent event) {
-        KerboScriptExpr expr = getSimplifiable(event);
-        if (expr != null) {
-            WriteCommandAction.runWriteCommandAction(getEventProject(event), () -> {
-                try {
-                    expr.replace(simplify(expr));
-                } catch (ActionFailedException e) {
-                    e.printStackTrace();
-                    Messages.showDialog(e.getMessage(), "Failed:", new String[]{"OK"}, -1, null);
-                }
-            });
+        final Project project = getEventProject(event);
+        if (project == null) return;
+        final Editor editor = event.getData(CommonDataKeys.EDITOR);
+        if (editor == null) return;
+        final KerboScriptExpr expr = getSimplifiable(event);
+        if (expr == null) return;
+        final KerboScriptExpr simplify;
+        try {
+            simplify = simplify(expr);
+        } catch (ActionFailedException e) {
+            final String message = "Cannot simplify expression '" + expr + "': " + e.getMessage();
+            LOG.warn(message, e);
+            HintManager.getInstance().showErrorHint(editor, message);
+            return;
         }
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            expr.replace(simplify);
+        });
     }
 
     private KerboScriptExpr simplify(KerboScriptExpr expr) throws ActionFailedException {
@@ -38,6 +53,7 @@ public class Simplify extends BaseAction {
             throw new ActionFailedException(e);
         }
     }
+
     @Override
     public void update(AnActionEvent event) {
         event.getPresentation().setEnabled(getSimplifiable(event) != null);
